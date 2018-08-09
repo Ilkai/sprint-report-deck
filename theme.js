4a072945d98168595c92e13c7579f0ac2a1da2aa
Lynden Noye <lt.noye@gmail.com>
2018-08-10 01:25:40 +1000

    Add more properties to theme

--- theme.js
+++ theme.js
@@ -2,9 +2,7 @@ import { theme } from 'mdx-deck/themes'
 
 export default {
   ...theme,
-  // add a custom font
-  font: '"Neue Haas Grotesk", sans-serif',
-  // custom colors
+  font: '"Open Sans", "Neue Haas Grotesk", sans-serif',
   colors: {
     text: '#222',
     background: '#fff',
@@ -12,5 +10,19 @@ export default {
   },
   css: {
     textAlign: 'left'
+  },
+  h1: {
+    fontSize: '2.5rem'
+  },
+  h2: {
+    fontSize: '1.5rem'
+  },
+  h3: {
+    fontSize: '1rem'
+  },
+  h4: {
+    fontSize: '0.75rem',
+    textTransform: 'uppercase',
+    letterSpacing: '0.25em'
   }
 }
